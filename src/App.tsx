c23fde71929d02d60a4793e966f1ecf6c7b74672
Yakov <lawrinov.yackov@yandex.ru>
2024-08-04 20:09:01 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { FC } from "react";
 import "./App.css";
 import { SummaryStatisticsDealers } from "./component/SummaryStatisticsDealers/SummaryStatisticsDealers";
 
-const App = () => {
+const App: FC = () => {
     return <SummaryStatisticsDealers />;
 };
 
